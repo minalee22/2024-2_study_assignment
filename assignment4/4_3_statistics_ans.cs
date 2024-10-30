497141d3f0c7195ecbb3ed1af2ee7d4994535015
minalee22 <87276148+minalee22@users.noreply.github.com>
2024-10-30 23:17:33 +0900

    Update 4_3_statistics_ans.cs

--- assignment4/4_3_statistics_ans.cs
+++ assignment4/4_3_statistics_ans.cs
@@ -19,9 +19,61 @@ namespace statistics
             // double.Parse(str)
 
             int stdCount = data.GetLength(0) - 1;
-            // ---------- TODO ----------
-            
-            // --------------------
+            int sjtCount = data.GetLength(1) - 2;
+
+            string[] names = new string[stdCount];
+            double[] total = new double[stdCount];
+            double[] avg = new double[sjtCount];
+            double[] max = new double[sjtCount];
+            double[] min = new double[sjtCount];
+
+            for (int j = 0; j < sjtCount; j++){
+                max[j] = double.MinValue;
+                min[j] = double.MaxValue;
+            }
+
+            for (int i = 1; i <= stdCount; i++)
+            {
+                double totalScore = 0;
+                names[i - 1] = data[i, 1];
+                for (int j = 2; j < sjtCount + 2; j++)
+                {
+                    double score = double.Parse(data[i, j]);
+                    totalScore += score;
+                    avg[j - 2] += score / stdCount;
+                    if (score > max[j - 2]) 
+                        max[j - 2] = score;
+                    if (score < min[j - 2]) 
+                        min[j - 2] = score;
+                }
+                total[i - 1] = totalScore;
+            }
+
+            Console.WriteLine("Average Scores:");
+            for(int j = 2; j < sjtCount + 2; j++){
+                Console.WriteLine($"{data[0, j]}: {avg[j - 2]:F2}");
+            }
+            Console.WriteLine("\nMax and min Scores:");
+            for(int j = 2; j < data.GetLength(1); j++){
+                Console.WriteLine($"{data[0, j]}: ({max[j - 2]}, {min[j - 2]})");
+            }
+
+            Array.Sort(total, names);
+            Array.Reverse(total);
+            Array.Reverse(names);
+
+            Console.WriteLine("\nStudents rank by total scores:");
+            for(int i = 0; i < stdCount; i++){
+                int rank = i + 1;
+                if(rank == 1)
+                    Console.WriteLine($"{names[i]}: {rank}st");
+                else if(rank == 2)
+                    Console.WriteLine($"{names[i]}: {rank}nd");
+                else if(rank == 3)
+                    Console.WriteLine($"{names[i]}: {rank}rd");
+                else
+                    Console.WriteLine($"{names[i]}: {rank}th");
+            }
         }
     }
 }
